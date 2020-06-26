96dcfdd397ab82993ae812f6a96a5bbfa8d858b5
Brian Kessler <kessler.bm@gmail.com>
2020-06-26 17:38:25 +0200

    Created demo for CustomEvent.

---
+++ force-app/main/default/lwc/eventWithData/eventWithData.js
@@ -0,0 +1,22 @@
+import {LightningElement, track, wire} from 'lwc';
+import getContacts from '@salesforce/apex/ContactController.getContacts';
+
+export default class EventWithData extends LightningElement {
+    @track
+    selectedContact;
+
+    @wire(getContacts)
+    contacts;
+
+    get listIsNotEmpty() {
+        return this.contacts
+            && Array.isArray(this.contacts.data)
+            && this.contacts.data.length > 0;
+    }
+
+    contactSelected(event) {
+        const contactId = event.detail;
+        this.selectedContact = this.contacts.data.find(contact => contact.Id === contactId);
+        console.log('####### this.selectedContact: ' + this.selectedContact);
+    }
+}
